46ffa079acd9b8ebee8b51dd18537adfa2aaec5b
stevendevpr <stevenbonilla46@yahoo.com>
2024-04-19 17:58:43 -0400

    fixing

--- shell.c
+++ shell.c
@@ -16,6 +16,16 @@ int main(void)
     char command_path[MAX_INPUT_LENGTH];
     pid_t pid;
 
+    FILE *input_file;
+
+    /* Open a temporary file for writing */
+    input_file = fopen("input.txt", "w");
+    if (input_file == NULL)
+    {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
     while (1)
     {
         /* Display prompt */
@@ -48,6 +58,9 @@ int main(void)
         /* Use the input as the command path */
         strncpy(command_path, input, MAX_INPUT_LENGTH);
 
+        /* Write input to the temporary file */
+        fprintf(input_file, "%s\n", input);
+
         /* Fork a new process */
         pid = fork();
         if (pid == -1)
@@ -58,6 +71,25 @@ int main(void)
         else if (pid == 0)
         {
             /* Child process */
+            /* Close the file descriptor for the input file */
+            fclose(input_file);
+
+            /* Open the temporary file for reading */
+            input_file = fopen("input.txt", "r");
+            if (input_file == NULL)
+            {
+                perror("fopen");
+                exit(EXIT_FAILURE);
+            }
+
+            /* Redirect stdin to the temporary file */
+            if (dup2(fileno(input_file), STDIN_FILENO) == -1)
+            {
+                perror("dup2");
+                exit(EXIT_FAILURE);
+            }
+
+            /* Execute the command */
             execl(command_path, input, (char *)NULL);
             /* If execl returns, it means command execution failed */
             perror(input);
@@ -72,8 +104,17 @@ int main(void)
                 perror("wait");
                 exit(EXIT_FAILURE);
             }
+
+            /* Rewind the input file */
+            rewind(input_file);
         }
     }
+
+    /* Close the input file */
+    fclose(input_file);
+
+    /* Remove the temporary file */
+    remove("input.txt");
 
     return EXIT_SUCCESS;
 }
