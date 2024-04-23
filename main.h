b12d538476f9c90ed51eac25d92b4198e8e7b4bf
Kryss <kryssbm99@gmail.com>
2024-04-23 13:42:02 -0400

    pushing to my branch

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <ctype.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+void display_shell_prompt(void);
+char *get_user_input(void);
+int has_whitespace(char *command);
+int execute_shell_command(char *command);
+int main(void);
+
+#endif
